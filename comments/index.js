a57d7c6d7442059f034ca852fd686243f0c85994
sylviawanjiku <sylviakatherine22@gmail.com>
2022-08-12 21:59:48 +0300

    add gitignore

--- comments/index.js
+++ comments/index.js
@@ -1,7 +1,8 @@
-const express  = require("express");
+const express = require("express");
 const bodyParser = require("body-parser");
+const axios = require("axios");
 const { randomBytes } = require("crypto");
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,14 +14,21 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
-  const {content} = req.body;
+  const { content } = req.body;
   const comments = commentsByPostId[req.params.id] || [];
 
   comments.push({ id: commentId, content });
   commentsByPostId[req.params.id] = comments;
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data:{
+      commentId,
+      content,
+      postId: req.params.id
+    }
+  })
   res.status(201).send(comments);
 });
 
